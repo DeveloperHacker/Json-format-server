c340271ceccba2d1c0e3d697f6a9af259d38ea67
MadCat <seryoga@computercraft.ru>
2017-10-21 15:58:53 +0300

    init

---
+++ src/main/java/Server.kt
@@ -0,0 +1,32 @@
+import com.google.gson.GsonBuilder
+import com.google.gson.JsonParser
+import com.sun.net.httpserver.HttpExchange
+import com.sun.net.httpserver.HttpHandler
+import com.sun.net.httpserver.HttpServer
+import java.net.InetSocketAddress
+
+class EchoHandler : HttpHandler {
+    override fun handle(exchange: HttpExchange) {
+        val response = try {
+            val parser = JsonParser()
+            val builder = GsonBuilder().setPrettyPrinting().create()
+            val string = exchange.requestBody.bufferedReader().readText()
+            val element = parser.parse(string)
+            builder.toJson(element) + "\n"
+        } catch (ex: Exception) {
+            ex.message + "\n"
+        }
+        exchange.sendResponseHeaders(200, response.length.toLong())
+        exchange.responseBody.write(response.toByteArray())
+        exchange.responseBody.close()
+    }
+}
+
+
+fun main(args: Array<String>) {
+    val server = HttpServer.create()
+    server.bind(InetSocketAddress(7777), 0)
+    server.createContext("/", EchoHandler())
+    server.executor = null
+    server.start()
+}
